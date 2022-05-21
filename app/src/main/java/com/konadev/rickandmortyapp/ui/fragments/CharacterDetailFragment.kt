bf7b8b12e164bd5eb0e2afda03b2e41809b37293
pablotomey <pablo.tp87@gmail.com>
2022-05-21 13:36:53 -0300

    setup nav graph and fragments

---
+++ app/src/main/java/com/konadev/rickandmortyapp/ui/fragments/CharacterDetailFragment.kt
@@ -0,0 +1,7 @@
+package com.konadev.rickandmortyapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.konadev.rickandmortyapp.R
+
+class CharacterDetailFragment: Fragment(R.layout.fragment_character_detail) {
+}
---
+++ app/src/main/java/com/konadev/rickandmortyapp/ui/fragments/CharactersFragment.kt
@@ -0,0 +1,7 @@
+package com.konadev.rickandmortyapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.konadev.rickandmortyapp.R
+
+class CharactersFragment: Fragment(R.layout.fragment_characters) {
+}
